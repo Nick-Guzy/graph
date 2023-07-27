98112b2944beacd715bfc00e21b1c719789ffdd1
Nick-Guzy <nick.guzy76@gmail.com>
2023-07-27 15:04:26 -0700

    Finished

--- src/graph.js
+++ src/graph.js
@@ -14,4 +14,28 @@ export default class Graph {
     }
     return false;
   }
+  createEdge(node1, node2) {
+    let set1 = this.adjacencyList.get(node1);
+    let set2 = this.adjacencyList.get(node2);
+    set1.add(node2);
+    set2.add(node1);
+  }
+  hasEdge(node1, node2) {
+    if (this.adjacencyList.get(node1).has(node2)) {
+      return true
+    }
+    return false;
+  }
+  removeEdge(node1, node2) {
+    this.adjacencyList.get(node1).delete(node2);
+    this.adjacencyList.get(node2).delete(node1);
+  }
+  removeNode(name) {
+    if (this.adjacencyList.has(name)) {
+      this.adjacencyList.get(name).forEach((edge) => {
+        this.adjacencyList.get(edge).delete(name);
+      });
+      this.adjacencyList.delete(name);
+    }
+  }
 }
