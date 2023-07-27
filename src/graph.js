a127ad06e77b17df83de84d84c8beeab4cf518d5
Nick-Guzy <nick.guzy76@gmail.com>
2023-07-27 14:59:29 -0700

    Creating graph creation

---
+++ src/graph.js
@@ -0,0 +1,17 @@
+export default class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+  addNode(name) {
+    this.adjacencyList.set(name, new Set());
+  }
+  hasNode(name) {
+    return false;
+  }
+  hasNode(name) {
+    if (this.adjacencyList.get(name)) {
+      return true;
+    }
+    return false;
+  }
+}
